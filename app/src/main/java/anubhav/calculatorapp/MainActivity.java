9735be2bc7bfc64188ca9a4bd179d3eb66713e79
Conor Hall <conorhall.96@gmail.com>
2018-04-27 11:24:02 +0100

    Demo

--- app/src/main/java/anubhav/calculatorapp/MainActivity.java
+++ app/src/main/java/anubhav/calculatorapp/MainActivity.java
@@ -2,13 +2,11 @@ package anubhav.calculatorapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.d
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
